16d3fe1b1ab72d79468a898983d51cc0ba9cd954
jessicamlee <jessicamlee25@gmail.com>
2023-05-29 23:08:10 -0700

    Setup default interface without quote

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const GradientBackgroundCon = styled.div`
-  background: linear-gradient(to right, #111169, #a7a7ff);
+  background: linear-gradient(to right, #0e2a6c, #9b9bfb);
   background-size: 400% 400%;
   animation: gradient 6s ease infinite;
   height: 100vh;
@@ -18,5 +18,152 @@ export const GradientBackgroundCon = styled.div`
     100% {
         background-position: 0% 50%;
     }
+  }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -10px;
+  margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: -80px;
+  bottom: 80px;
+`;
+
+export const FooterCon = styled.footer`
+  width: 100vw;
+  height: auto;
+  padding: 20px 0;
+  text-align: center;
+  font-family: 'Manrope', sans-serif;
+  letter-spacing: 2px;
+  font-weight: 400;
+  font-size: 18px;
+  position: absolute;
+  bottom: 0;
+  color: #fff;
+  z-index: 2;
+  @media only screen and (max-width: 600px) {
+    font-size: 16px;
+  }
+`;
+
+export const FooterLink = styled(Link)`
+  color: #fff;
+`;
+
+export const QuoteGeneratorCon = styled.div`
+  min-height: 350px;
+  min-width: 350px;
+  height: 60vh;
+  width: 70vw;
+  border: 2px solid #ffffff22;
+  border-radius: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 2;
+  padding: 0 20px;
+
+  /* hype4academy's Glass Morphism Style: https://hype4.academy/tools/glassmorphism-generator */
+  background: rgba( 0, 0, 70, 0.3 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 2.5px );
+  -webkit-backdrop-filter: blur( 2.5px );
+  border-radius: 10px;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+`;
+
+export const QuoteGeneratorTitle = styled.h2`
+  font-family: 'Manrope', sans-serif;
+  letter-spacing: 2px;
+  font-weight: 200;
+  font-size: 48px;
+  text-align: center;
+  color: #fff;
+  padding: 0px 20px;
+  position: relative;
+  @media only screen and (max-width: 600px) {
+    font-size: 36px;
+    line-height: 44px;
+  }
+`;
+
+export const QuoteGeneratorSubTitle = styled.p`
+  color: #fff;
+  font-family: 'Manrope', sans-serif;
+  font-weight: 400;
+  font-size: 24px;
+  margin: 16px 0;
+  position: relative;
+  width: 100%;
+  text-align: center;
+  padding: 0px 20px;
+  @media only screen and (max-width: 600px) {
+    font-size: 18px;
+    line-height: 28px;
+  }
+`;
+
+export const GenerateQuoteButton = styled.div`
+  height: 60px;
+  width: 250px;
+  border: 2px solid darkgrey;
+  border-radius: 20px;
+
+  margin-top: 20px;
+  position: relative;
+  transition: 0.2s all ease-in-out;
+  cursor: pointer;
+  top: 20px;
+  margin: auto;
+  transform-origin: center;
+
+  /* hype4academy's Glass Morphism Effect */
+  background: rgba(0,0,70, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.2);
+  backdrop-filter: blur(1.5);
+  -webkit-backdrop-filter: blur(1.5);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.8);
+
+  &:hover {
+    filter: brightness(3);
+    transition: 0.2s all ease-in-out;
+    transform: scale(1.1);
+
+    transform-origin: center;
+  }
+`;
+
+export const GenerateQuoteButtonText = styled.div`
+  color: #fff;
+  font-family: 'Manrope', sans-serif;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 20px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  @media only screen and (max-width: 600px) {
+    font-size: 17px;
   }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+import React, { useState } from 'react';
+
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+
+// Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GenerateQuoteButton, GenerateQuoteButtonText, GradientBackgroundCon, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorSubTitle, QuoteGeneratorTitle } from '@/components/QuoteGenerator/QuoteGeneratorElements';
+
+// Assets
+import Clouds1 from '@/assets/cloud-and-thunder.png';
+import Clouds2 from '@/assets/cloudy-weather.png';
 
 export default function Home() {
+  // UseState Hook to save the value to numberOfQuotes
+  // Set the type as either "Number" or "null"
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -13,6 +25,57 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
+
+        {/* Quote Generator Modal: Pop-up to call our script*/}
+        {/* <QuoteGeneratorModal 
+        /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Want to be inspired? Get a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+                Get your Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="Cloud with thunderbolt 3D graphic"
+        />
+
+        <BackgroundImage2 
+          src={Clouds2}
+          height="230"
+          alt="Partly cloudy 3D graphic with sun behind cloud"
+        />
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+            {/* For counting the number of quotes generated in the back-end */}
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with 💛 by <FooterLink 
+              href="https://jessicamlee.dev" 
+              target="_blank" 
+              // Security Purpose: Helps with legacy browsers regarding web vulnerabilities
+              rel="noopener noreferrer"
+              > Jessica Lee </FooterLink>
+          </>
+        </FooterCon>
 
       </GradientBackgroundCon>
     </>
